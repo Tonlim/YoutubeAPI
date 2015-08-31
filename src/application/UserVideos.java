99d41ed420c0251de50724be26b4d8684a7553fa
Nick Aelterman <nick.aelterman@ugent.be>
2015-08-31 22:38:47 +0200

    get last 50 uploaded vids
    TODO: find a way to get ALL uploaded vids

--- src/application/UserVideos.java
+++ src/application/UserVideos.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,6 +11,8 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Channel;
 import com.google.api.services.youtube.model.ChannelListResponse;
+import com.google.api.services.youtube.model.PlaylistItem;
+import com.google.api.services.youtube.model.PlaylistItemListResponse;
 import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.Thumbnail;
@@ -22,11 +25,13 @@ public class UserVideos {
 		Channel channel;
 		
 		try {
+			//make the structure to handle the requests
 			youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
                 public void initialize(HttpRequest request) throws IOException {
                 }
             }).setApplicationName("youtube-etho-search").build();
 			
+			//get Etho's uploaded videos playlist ID
 			YouTube.Channels.List search = youtube.channels().list("id,snippet,contentDetails");
 			
 			search.setKey(API_KEY);			
@@ -34,9 +39,27 @@ public class UserVideos {
 			
 			ChannelListResponse searchResponse = search.execute();
 			List<Channel> results = searchResponse.getItems();
-			if(results != null){
-				prettyPrint(results);
+			String uploadsID = results.get(0).getContentDetails().getRelatedPlaylists().getUploads();
+			
+			//get videoIDs in playlist
+			YouTube.PlaylistItems.List playlistitems = youtube.playlistItems().list("id,snippet,contentDetails");
+			playlistitems.setKey(API_KEY);
+			playlistitems.setPlaylistId(uploadsID);
+			playlistitems.setMaxResults((long) 50);
+			
+			PlaylistItemListResponse playlistResponse = playlistitems.execute();
+			List<PlaylistItem> items = playlistResponse.getItems();
+			List<String> videoIDs = new ArrayList<String>();
+			for(PlaylistItem i : items){
+				videoIDs.add(i.getContentDetails().getVideoId());
 			}
+			
+			System.out.println(videoIDs.get(0));
+			System.out.println(videoIDs.get(1));
+			System.out.println(videoIDs.get(2));
+			
+			System.out.println("\n"+videoIDs.size());
+			
 			
 			
 		} catch (GoogleJsonResponseException e) {
@@ -55,8 +78,5 @@ public class UserVideos {
 	
 	private static void prettyPrint(List<Channel> results) {
 		System.out.println(results.get(0).getSnippet());
-		String uploadsID = results.get(0).getContentDetails().getRelatedPlaylists().getUploads();
-		
-        System.out.println(uploadsID);
     }
 }
